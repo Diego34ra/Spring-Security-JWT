e9751706a1d26e1ef1c20ccb426b74b2ec130533
Diego Ribeiro <diegoribeiro13ra@hotmail.com>
2022-06-17 22:17:06 -0300

    Commit inicial

---
+++ curso-spring-security-jwt/src/test/java/curso/spring/security/jwt/CursoSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.spring.security.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
